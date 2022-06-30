e9b4d0ba2a3fef00108e2cc19b3fc7a9581bd967
daniel tate <daniel.tate@gmail.com>
2022-06-30 13:52:14 -0500

    Build 2

--- check_phpfpm_slowreq.py
+++ check_phpfpm_slowreq.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python
-""" Check php_status page for slow requests and report back.  Internal use. (c) 2022 Daniel Tate
+#!/usr/bin/python -O
+""" Check php_status page for slow requests and report back.  Internal use. (c) 2022 Daniel Tate v1.0 \
+build 2
 """
 
 import argparse
@@ -7,6 +8,17 @@ import urllib.request
 import json
 import urllib
 import shutil
+import os
+from time import perf_counter
+from datetime import datetime
+from datetime import timedelta
+from os.path import exists
+
+tmp_dir = "/tmp/phpfpm_diff"
+
+
+differential_dir = tmp_dir + "/"
+
 
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -22,7 +34,7 @@ parser.add_argument('-c', '--crit', help="crit threshold", metavar="crit", type=
 parser.add_argument('-w', '--warn', help="warn threshold", metavar="warn", type=int)
 parser.add_argument('-s', '--site', help="warn threshold", metavar="warn")
 parser.add_argument('-d', '--diff', help="Differential Mode", action='store_true')
-parser.add_argument('-t', '--time', help="Time", metavar="time")
+parser.add_argument('-t', '--time', help="Time", metavar="time", type=int)
 args = parser.parse_args()
 
 def get_status ():
@@ -60,7 +72,7 @@ def get_aged_reqs (difffile):
             reqcount=my_list[1]
             if __debug__:
                 print("DEBUG: SUB: aged_reqs: my_list: ",my_list)
-                print("DEBUG: SUB: aged_reqs: reqcount: ",bancount)
+                print("DEBUG: SUB: aged_reqs: reqcount: ",reqcount)
             dfile.close()
             return reqcount
 
@@ -73,7 +85,7 @@ def rotate():
     if __debug__: print("DEBUG: DIFF: POSTROTATE: Creating new datafile...")
     with open(differential_dir + "00m", 'w') as f00m:
         str_assemble1 = str(perf_counter())
-        str_assemble2 = str(str(count))
+        str_assemble2 = str(str(slow_req))
         str_assemble = str_assemble1 + " " + str_assemble2
         test = f00m.write(str_assemble)
         if __debug__: print("DEBUG: DIFF: POSTROTATE: New Create is:", test)
@@ -86,7 +98,7 @@ def setup_env ():
         print("DEBUG: DIFF: Creating initial file")
     with open(differential_dir + "00m", 'w') as f00m:
         str_assemble1 = str(perf_counter())
-        str_assemble2 = str(str(count))
+        str_assemble2 = str(str(slow_req))
         str_assemble = str_assemble1 + " " + str_assemble2
         write = f00m.write(str(str_assemble))
         if __debug__: print("DEBUG: DIFF: write is", write)
@@ -96,41 +108,41 @@ def setup_env ():
         shutil.copy(differential_dir + "00m", differential_dir + "45m")
         shutil.copy(differential_dir + "00m", differential_dir + "60m")
 
-def validate_differential ():
-    if int(aged_bans) == count:
+def validate_differential (old, new, current):
+    if int(old) == slow_req:
         if __debug__:
-            print(f"OK: Bans Unchanged {int(new_bans)} == {new_bans}|count={count}")
+            print(f"OK: Bans Unchanged {int(new)} == {new}|slow_req={current}")
             exit(0)
         else:
-            print(f"OK: Bans Unchanged {int(new_bans)} new in {args.time} min ({count})|new_bans={new_bans};{args.warn};{args.crit}")
+            print(f"OK: Bans Unchanged {int(new)} new in {args.time} min ({current})|new_reqs={new};{args.warn};{args.crit}")
             exit(0)
-    elif int(aged_bans) > count:
+    elif int(old) > slow_req:
         if __debug__:
-            print(f"OK: Decrease in bans {int(aged_bans)} > {count}")
+            print(f"OK: Decrease in bans {int(old)} > {current}")
             exit(0)
         else:
-            print(f"OK: Decrease in bans from {int(aged_bans)} to {count} |new_bans={new_bans};{args.warn};{args.crit}")
+            print(f"OK: Decrease in bans from {int(old)} to {current} |new_reqs={new};{args.warn};{args.crit}")
             exit(0)
-    elif (new_bans < args.warn):
+    elif (old < args.warn):
         if __debug__:
-            print(f"OK: {new_bans} new in {args.time} is less than {args.warn} new in {args.time}")
+            print(f"OK: {new} new in {args.time} is less than {args.warn} new in {args.time}")
             exit(0)
         else:
-            print(f"OK: {new_bans} new bans in {args.time} mins ({count})|new_bans={new_bans};{args.warn};{args.crit}")
+            print(f"OK: {new} new bans in {args.time} mins ({current})|new_reqs={new};{args.warn};{args.crit}")
             exit(0)
-    elif (new_bans >= args.warn and new_bans < args.crit):
+    elif (old >= args.warn and new < args.crit):
         if __debug__:
-            print( f"WARNING: {new_bans} new in {args.time} is greater than or equal to warn: {args.warn} new in {args.time}")
+            print( f"WARNING: {new} new in {args.time} is greater than or equal to warn: {args.warn} new in {args.time}")
             exit(1)
         else:
-            print(f"WARNING: {new_bans} new bans in {args.time} mins ({count})|new_bans={new_bans};{args.warn};{args.crit}")
+            print(f"WARNING: {new} new bans in {args.time} mins ({current})|new_reqs={new};{args.warn};{args.crit}")
             exit(1)
     else:
         if __debug__:
-            print(f"CRITICAL: bans: {new_bans} new in {args.time} is greater than crit: {args.crit} new in {args.time}")
+            print(f"CRITICAL: bans: {new} new in {args.time} is greater than crit: {args.crit} new in {args.time}")
             exit(2)
         else:
-            print(f"CRITICAL: bans: {new_bans} new bans in in {args.time} mins ({count})|new_bans={new_bans};{args.warn};{args.crit}")
+            print(f"CRITICAL: bans: {new} new bans in in {args.time} mins ({current})|new_reqs={new};{args.warn};{args.crit}")
             exit(2)
 
 def validate_normal (reqs):
@@ -193,6 +205,44 @@ elif args.warn > args.crit:
 
 json_data = get_status()
 slow_req = process_json(json_data)
-validate_normal(int(slow_req))
 
-print(f"Slow Requests={slow_req}|slow_requests={slow_req}")
+
+# Detect if we are in differential mode.
+if __debug__:print(f"DEBUG: DIFF: args.diff is True: {args.diff is True}")
+if args.diff is True:
+    if __debug__: print("DEBUG: DIFF: Differential is ON")
+
+### Do Setup if Needed.
+
+    dir_exists = os.path.exists(differential_dir)
+    if __debug__: print(f"DEBUG: DIFF: dir_exists: {dir_exists}")
+    if not dir_exists:
+        setup_env()
+
+if __debug__:
+    print("DEBUG: normal mode is:", normal_mode)
+    print("DEBUG: normal logic test is:", (normal_mode == 1))
+
+### File Aging
+if (normal_mode == 0):
+
+    if __debug__:
+        print(f"DEBUG: DIFF: DIFFERENTIAL")
+        print(f"DEBUG: Aging...")
+        print("DEBUG: DIFF: ROTATE: File 00m: ",file_age(differential_dir + "00m",timedelta(minutes=args.time)))
+    if file_age(differential_dir + "00m",timedelta(minutes=args.time)):
+        rotate() # will prv file w/ bans={log|wc-l}
+        aged_reqs = get_aged_reqs(differential_dir + str(args.time) +"m")
+        new_reqs = (slow_req - int(aged_reqs))
+        validate_differential(aged_reqs,new_reqs,slow_req)
+
+    else:
+        if __debug__: print("DEBUG: DIFF: NOAGE: File is not yet aged out.")
+        aged_reqs = get_aged_reqs(differential_dir + str(args.time) +"m")
+        new_reqs = (slow_req - int(aged_reqs))
+        validate_differential(aged_reqs,new_reqs,slow_req)
+
+if (normal_mode == 1):
+    if __debug__: print(f"In NORMAL MODE {normal_mode}")
+    validate_normal(int(slow_req))
+
